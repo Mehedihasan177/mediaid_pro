c1aba0a0713982ee16c2fcb4af92e5aaae46ff25
skabirgithub <abirshir1996@gmail.com>
2021-12-24 05:41:10 +0600

    tuk tak done

--- lib/views/widgets/notification_widget/notification_widget.dart
+++ lib/views/widgets/notification_widget/notification_widget.dart
@@ -33,7 +33,7 @@ Widget NotificationWidget(NotificationRes notification) =>
 
 
           Expanded(
-            flex: 3,
+            flex: 6,
             child: Padding(
               padding: const EdgeInsets.only(left: 15),
               child: Column(
